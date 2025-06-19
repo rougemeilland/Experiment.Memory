8cc2c5280bebe6863aa754f9d2cfec2a3aceb515
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2025-06-20 07:25:54 +0900

    Initial build

---
+++ Experiment.Library/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToSingleCore(System.Byte[],System.Int32,System.Boolean)~System.Single")]
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToUnmanagedTypeCore``1(System.Byte*,System.Boolean)~``0")]
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToInt16LE(System.ReadOnlySpan{System.Byte})~System.Int16")]
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToInt16BE(System.ReadOnlySpan{System.Byte})~System.Int16")]
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToUnmanagedTypeCore``1(``0@,System.Boolean)~``0")]
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToUnmanagedTypeCore``1(System.Byte@,System.Boolean)~``0")]
+[assembly: SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>", Scope = "member", Target = "~M:Experiment.Library.MemoryTest.ToInt128BE(System.ReadOnlySpan{System.Byte})~System.Int128")]
---
+++ Experiment.Library/MemoryTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Buffers.Binary;
+
+namespace Experiment.Library
+{
+    public static class MemoryTest
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static Int16 ToInt16LE(ReadOnlySpan<Byte> array)
+        {
+            if (array.Length < sizeof(Int16))
+                throw new ArgumentOutOfRangeException(nameof(array), "Not enough bytes in the array to read Int16.");
+
+            unsafe
+            {
+                fixed (Byte* p = array)
+                {
+                    return ToUnmanagedTypeCore<Int16>(ref *p, true);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static Int16 ToInt16BE(ReadOnlySpan<Byte> array)
+        {
+            if (array.Length < sizeof(Int16))
+                throw new ArgumentOutOfRangeException(nameof(array), "Not enough bytes in the array to read Int16.");
+
+            unsafe
+            {
+                fixed (Byte* p = array)
+                {
+                    return ToUnmanagedTypeCore<Int16>(ref *p, true);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0049:名前を単純化します", Justification = "<保留中>")]
+        public static Int128 ToInt128LE(ReadOnlySpan<Byte> array)
+        {
+            if (array.Length < 16)
+                throw new ArgumentOutOfRangeException(nameof(array), "Not enough bytes in the array to read Int16.");
+
+            unsafe
+            {
+                fixed (Byte* p = array)
+                {
+                    return ToUnmanagedTypeCore<Int16>(ref *p, true);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static Int128 ToInt128BE(ReadOnlySpan<Byte> array)
+        {
+            if (array.Length < 16)
+                throw new ArgumentOutOfRangeException(nameof(array), "Not enough bytes in the array to read Int16.");
+
+            unsafe
+            {
+                fixed (Byte* p = array)
+                {
+                    return ToUnmanagedTypeCore<Int16>(ref *p, true);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        private static unsafe VALUE_T ToUnmanagedTypeCore<VALUE_T>(Byte* buffer, Boolean convertFromLittleEndian)
+            where VALUE_T : unmanaged
+        {
+            if (BitConverter.IsLittleEndian == convertFromLittleEndian)
+                return Unsafe.Read<VALUE_T>(buffer);
+            VALUE_T value;
+            Unsafe.CopyBlockUnaligned(&value, buffer, (UInt32)sizeof(VALUE_T));
+            BinaryPrimitives.ReverseEndianness(new Span<VALUE_T>(ref value));
+            return value;
+        }
+
+        private static unsafe void ReverseByteArray<VALUE_T>(VALUE_T* value)
+            where VALUE_T : unmanaged
+        {
+            var p = (byte*)&value;
+            if (typeof(VALUE_T) == typeof(sbyte))
+            {
+            }
+            else if (typeof(VALUE_T) == typeof(byte))
+            {
+            
+            short
+                    ushort
+                    }
+            int
+                uint
+                long
+                Int128
+                UInt128
+            ulong
+                float
+                double
+                decimal
+
+
+
+
+            :
+                    break;
+                case 2:
+                    BinaryPrimitives.ReverseEndianness<VALUE_T>(ref *value);
+                    (p[1], p[0]) = (p[0], p[1]);
+                    break;
+                case 4:
+                    (p[3], p[0]) = (p[0], p[3]);
+                    (p[2], p[1]) = (p[1], p[2]);
+                    break;
+                case 8:
+                    (p[7], p[0]) = (p[0], p[7]);
+                    (p[6], p[1]) = (p[1], p[6]);
+                    (p[5], p[2]) = (p[2], p[5]);
+                    (p[4], p[3]) = (p[3], p[4]);
+                    break;
+                case 16:
+                    (p[7], p[0]) = (p[0], p[7]);
+                    (p[6], p[1]) = (p[1], p[6]);
+                    (p[5], p[2]) = (p[2], p[5]);
+                    (p[4], p[3]) = (p[3], p[4]);
+                    (p[7], p[4]) = (p[4], p[7]);
+                    (p[6], p[5]) = (p[5], p[6]);
+                    (p[5], p[6]) = (p[6], p[5]);
+                    (p[4], p[7]) = (p[7], p[4]);
+                    break;
+                default:
+                    throw new NotSupportedException($"Unsupported size of VALUE_T: {sizeof(VALUE_T)} bytes.");
+            }
+        }
+    }
+}
